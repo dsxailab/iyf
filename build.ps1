9c66aeb2dbbf253932d1d084383555e88dd4f086
dsxailab <noreply@dsxailab.net>
2026-02-28 17:22:55 +0800

    remove auto gen apks

--- build.ps1
+++ build.ps1
@@ -1,3 +1,8 @@
+rm -f ./final-signed.apk
+rm -f ./iyf.apk
+rm -f ./output-aligned.apk
 apktool b ./1-1770982127 -o iyf.apk
 zipalign -v 4 iyf.apk output-aligned.apk   
 apksigner sign --ks my-release-key.jks --out final-signed.apk output-aligned.apk
+rm -f ./iyf.apk
+rm -f ./output-aligned.apk
