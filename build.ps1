825f2e75983fd5f241c0c9c944ac7c37860e2c01
dsxailab <noreply@dsxailab.net>
2026-04-22 13:10:07 +0800

    add timestamp for apk to distinguish version
    as the tv browser app won't download again if the name is the same

--- build.ps1
+++ build.ps1
@@ -1,8 +1,12 @@
-rm -f ./final-signed.apk
+$timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+$finalApk = "./final-signed-$timestamp.apk"
+
+rm -f ./final-signed-*.apk
 rm -f ./iyf.apk
 rm -f ./output-aligned.apk
 apktool b ./1-1770982127 -o iyf.apk
 zipalign -v 4 iyf.apk output-aligned.apk   
-apksigner sign --ks my-release-key.jks --out final-signed.apk output-aligned.apk
+apksigner sign --ks my-release-key.jks --ks-pass pass:111111 --out $finalApk output-aligned.apk
 rm -f ./iyf.apk
 rm -f ./output-aligned.apk
+rm -f ./*.idsig
